c63902d9f8a931398be45710a09b0b22378bbb73
Kevin Digan <kd073@hotmail.com>
2021-09-10 15:05:27 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+main.remove();
+ 
+const element = document.createElement('h1');
+document.body.appendChild(element);
+const h1 = document.createElement('h1');
+
+let newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.innerHTML = 'Kevin is the champion';
